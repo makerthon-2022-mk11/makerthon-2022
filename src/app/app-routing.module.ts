70c3a465e93dbc4cbfecee6d9c75fa52a29883e1
Geng Ning <gengninghan1@gmail.com>
2021-12-11 21:17:48 +0800

    Add login page and avatar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomePageModule),
   },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginPageModule),
+  },
 ];
 
 @NgModule({
