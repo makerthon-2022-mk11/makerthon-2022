02e203f407d539a59d9f99162c3c24dba1db620a
Geng Ning <gengninghan1@gmail.com>
2021-12-26 15:30:36 +0800

    Add machine animation

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -7,11 +7,44 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  machineImages: HTMLImageElement[];
+  machineUrl: string;
+  baseUrl: string;
+  knobUrl: string;
+
   constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.initializeMachineImages();
+    this.machineUrl = this.machineImages[0].src;
+    this.baseUrl = '../../../assets/images/gacha/base.png';
+    this.knobUrl = '../../../assets/images/gacha/knob.png';
+  }
 
   get displayName(): string {
     return this.userService.user?.displayName;
+  }
+
+  animateMachine(index: number = 0) {
+    this.machineUrl = this.machineImages[index].src;
+    setTimeout(() => {
+      if (index < this.machineImages.length - 1) {
+        this.animateMachine(index + 1);
+      } else {
+        this.machineUrl = this.machineImages[0].src;
+      }
+    }, 300);
+  }
+
+  // Preload images so that transition will be smooth
+  initializeMachineImages() {
+    const numImages = 10;
+    this.machineImages = new Array(10);
+    for (let i = 0; i < numImages; i++) {
+      this.machineImages[i] = new Image();
+      this.machineImages[
+        i
+      ].src = `../../../assets/images/gacha/gacha-machine/${i}.png`;
+    }
   }
 }
